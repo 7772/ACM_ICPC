5366259d4ebb15aab77a34241fde48acef52038e
7772 <sbshz85@gmail.com>
2018-07-23 01:49:19 +0900

    commit avoid overite operation

--- DP/9095.cpp
+++ DP/9095.cpp
@@ -9,6 +9,7 @@
 
 using namespace std;
 
+// 일반적인 방법
 int main() {
     int DP[11];
     int i, n, T;
@@ -29,3 +30,27 @@ int main() {
 
     return 0;
 }
+
+// 중복 계산을 피하기 위해
+// 미리 DP 연산을 끝냄
+// int main() {
+//     int DP[11];
+//     int i, n, T;
+
+//     DP[0] = 1;
+//     DP[1] = 1;
+//     DP[2] = 2;
+
+//     cin >> T;
+
+//     for (i = 3; i <= 11; i++) {
+//         DP[i] = DP[i - 1] + DP[i - 2] + DP[i - 3];
+//     }
+
+//     while ((T--)) {
+//         cin >> n;
+//         cout << DP[n] << endl;
+//     }
+
+//     return 0;
+// }
