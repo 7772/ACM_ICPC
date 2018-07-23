6417a4b20786514d14accae82722377255e5edc4
7772 <sbshz85@gmail.com>
2018-07-23 15:03:17 +0900

    commit 2193

---
+++ DP/2193.cpp
@@ -0,0 +1,32 @@
+/**
+ * @site: https://www.acmicpc.net/problem/2193
+ * @github: https://github.com/7772
+ * @auth: Landon Park
+ * @date: 2018. 07. 24
+ */
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    signed long DP[91][2] = {0,};
+    signed long ans = 0;
+    int i, N;
+
+    cin >> N;
+
+    DP[1][0] = 0;
+    DP[1][1] = 1;
+
+    for (i = 2; i <= N; i++) {
+        DP[i][0] = DP[i - 1][0] + DP[i - 1][1];
+        DP[i][1] = DP[i - 1][0];
+    }
+
+    ans = DP[N][0] + DP[N][1];
+
+    cout << ans << endl;
+
+    return 0;
+}
