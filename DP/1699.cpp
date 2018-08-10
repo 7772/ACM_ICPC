cebc27e81da16fd0523d3fa96f44d18ca2d3b988
7772 <sbshz85@gmail.com>
2018-08-10 16:03:38 +0900

    update 1699

--- DP/1699.cpp
+++ DP/1699.cpp
@@ -8,10 +8,6 @@
 
 using namespace std;
 
-int min(int a, int b) {
-    return (a < b) ? (a) : (b);
-}
-
 int main() {
     int DP[100001];
     int i, j, N;
@@ -21,7 +17,9 @@ int main() {
     for (i = 1; i <= N; i++) {
         DP[i] = i;
         for (j = 2; j * j <= i; j++) {
-            DP[i] = min(DP[i], DP[i - (j * j)] + 1);
+            if (DP[i] > DP[i - (j * j)] + 1) {
+                DP[i] = DP[i - (j * j)] + 1;
+            }
         }
     }
 
